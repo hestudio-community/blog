6ee40c70c7f42f9c8b072a7a8f1ec11a967a0859
醉、倾城 <zihengdie@hestudio.net>
2023-08-12 03:25:25 +0000

    1

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,10 +1,10 @@
 // .vuepress/client.ts
 import { defineClientConfig } from "@vuepress/client";
-// import { defineWalineOptions } from "vuepress-plugin-comment2";
+import { defineWalineOptions } from "vuepress-plugin-comment2";
 
-// defineWalineOptions({
-//     // 选项
-// });
+defineWalineOptions({
+    // 选项
+});
 
 export default defineClientConfig({
     // ...
