2c84cea6b06d736bbf190c55b090131a2989e61b
醉、倾城 <zihengdie@hestudio.net>
2023-08-19 09:49:54 +0000

    1

---
+++ src/.vuepress/.components/NormalPage.vue
@@ -0,0 +1,10 @@
+<template>
+    <HopeNormalPage>
+        <template #contentBefore>
+            <Share colorful />
+        </template>
+    </HopeNormalPage>
+</template>
+<script setup lang="ts">
+import HopeNormalPage from "vuepress-theme-hope/components/NormalPage.ts";
+</script>
--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,8 +1,11 @@
+import { getDirname, path } from '@vuepress/utils';
 import { defineUserConfig } from "vuepress";
 import { searchPlugin } from "@vuepress/plugin-search";
 import { googleAnalyticsPlugin } from "@vuepress/plugin-google-analytics";
 import { removePWAPlugin } from "vuepress-plugin-remove-pwa";
 import theme from "./theme.js";
+
+const __dirname = getDirname(import.meta.url);
 
 export default defineUserConfig({
   base: "/",
@@ -16,6 +19,14 @@ export default defineUserConfig({
   },
 
   theme,
+
+  alias: {
+    // 你可以在这里将别名定向到自己的组件
+    "@theme-hope/components/NormalPage": path.resolve(
+      __dirname,
+      "./components/NormalPage.vue",
+    ),
+  },
 
   plugins: [
     // socialSharePlugin({
